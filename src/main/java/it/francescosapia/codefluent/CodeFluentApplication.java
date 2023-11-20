40ab2b0570155792f1152f73d8f3cdae471d5075
Francesco Sapia <123062954+sapiaf@users.noreply.github.com>
2023-11-20 14:46:22 +0100

    Project setup

---
+++ src/main/java/it/francescosapia/codefluent/CodeFluentApplication.java
@@ -0,0 +1,13 @@
+package it.francescosapia.codefluent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFluentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFluentApplication.class, args);
+	}
+
+}
